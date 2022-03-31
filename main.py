3cb493720691917608910d24418dc6a306691488
Audrey Sia <102685458+furiousemostalker@users.noreply.github.com>
2022-03-31 03:07:53 +0000

    made the thing but no have gravity big bruh

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("TEST")
+class Player(pygame.sprite.Sprite):
+  def __init__(self):
+    super().__init__()
+    self.image = pygame.image.load("reDinoTWO.png") #loads in reDinoTWO.png for the sprite
+    self.rect = self.image.get_rect() #makes the hitbox around the sprite image (I think??) 
+    self.rect.center = (50, 230) #sets the position of the sprite
+    self.isJump = False 
+    
+  def jump (self): #incomplete because I need to add gravity !!
+    if keys[pygame.K_SPACE]: #if the spacebar is being pressed
+      self.isJump = True
+      self.rect.y -= 25 #moves sprite up 25 pixels each time
+      self.rect.x += 5 #moves sprite right 25 pixels each time
+    
+  def draw (self, surface): #draws the sprite on the display
+    surface.blit(self.image, self.rect)
