25363d703bcb9cfc72a4f9f87f10f2a5d21613f3
Audrey Sia <102685458+furiousemostalker@users.noreply.github.com>
2022-04-07 17:41:56 +0000

    #comments

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ class Player(pygame.sprite.Sprite):
     self.rect.center = (50, 230) #sets the position of the sprite
     self.isJump = False 
     
-  def jump (self): #incomplete because I need to add gravity !!
+  def jump (self): 
     if keys[pygame.K_SPACE]: #if the spacebar is being pressed
       self.isJump = True #going to be used for gravity
-      self.rect.y -= 50 #moves sprite up 33 pixels each time
+      self.rect.y -= 50 #moves sprite up 50 pixels each time
 
   def gravity (self):
     if self.isJump == True:
-      self.rect.y += 7.5 #enables sprite to fall back down/not stay in the air
+      self.rect.y += 7.5 #enables sprite to fall back down/not stay in the air 
     
   def draw (self, surface): #draws the sprite on the display
     surface.blit(self.image, self.rect)
