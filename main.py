6ef74996fba4dfa8d0f11472ea4c8f413558cf20
Tri <tbui931958@gmail.com>
2022-04-21 18:04:55 +0000

    fix merge errors

--- main.py
+++ main.py
@@ -16,10 +16,28 @@ class Character(pygame.sprite.Sprite):
   # initialize character 
   def __init__(self):
     super().__init__()
-    self.image = pygame.image.load("images/Dino_Sprite/png/Character_Run.png")
-    self.rect = self.image.get_rect()
-    self.rect.center = (10,650)
+    # self.image = pygame.image.load("images/Dino_Sprite/png/Character_Run.png")
+    self.image = pygame.image.load("reDinoTWO.png")
+    self.rect = self.image.get_rect() #makes the hitbox around the sprite image (I think??) 
+    self.rect.center = (50, 330) #sets the position of the sprite
+    self.isJump = False 
 
+  def jump (self): 
+    if keys[pygame.K_SPACE]: #if the spacebar is being pressed
+      self.isJump = True #going to be used for gravity
+      self.rect.y -= 50 #moves sprite up 50 pixels each time
+
+  def gravity (self):
+    if self.isJump == True:
+      self.rect.y += 7.5 #enables sprite to fall back down/not stay in the air 
+
+  # def draw (self, surface): #draws the sprite on the display
+    # surface.blit(self.image, self.rect)  
+    # self.image = pygame.image.load("images/Dino_Sprite/png/Character_Run.png")
+    # self.image = pygame.image.load("reDinoTWO.png")
+    # self.rect = self.image.get_rect()
+    # self.rect.center = (10,400)
+    
 # background class
 class background():
   #initialize background image
@@ -79,12 +97,12 @@ def main():
         pygame.quit()
         sys.exit()
 
+    back_ground.update()
+    back_ground.render()
+    
     for sprite in all_sprites:
       displaysurface.blit(sprite.image, sprite.rect)
           
-    back_ground.update()
-    back_ground.render()
-
     pygame.display.update()
     FRAMEPERSEC.tick(FPS)
 
